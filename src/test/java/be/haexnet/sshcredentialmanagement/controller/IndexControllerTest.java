3272c550cfc10cbbf1c1c122f3532db0048707e5
martijnhaex <martijn.haex@c4j.be>
2014-09-15 16:40:51 +0200

    Test IndexController.

--- src/test/java/be/haexnet/sshcredentialmanagement/controller/IndexControllerTest.java
+++ src/test/java/be/haexnet/sshcredentialmanagement/controller/IndexControllerTest.java
@@ -1,7 +1,39 @@
 package be.haexnet.sshcredentialmanagement.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(MockitoJUnitRunner.class)
 public class IndexControllerTest {
+    MockMvc mockMvc;
 
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = standaloneSetup(new IndexController()).build();
+    }
+
+    @Test
+    public void indexStatus() throws Exception {
+        doGetIndex()
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void indexForward() throws Exception {
+        doGetIndex()
+                .andExpect(forwardedUrl("index"));
+    }
+
+    private ResultActions doGetIndex() throws Exception {
+        return mockMvc.perform(get("/"));
+    }
 }
