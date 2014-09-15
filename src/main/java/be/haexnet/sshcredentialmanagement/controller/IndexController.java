6382a0035057cd6b8fb8098d4f5204cc17d61f8a
martijnhaex <martijn.haex@c4j.be>
2014-09-15 16:45:20 +0200

    Introduce CredentialService in IndexController.

--- src/main/java/be/haexnet/sshcredentialmanagement/controller/IndexController.java
+++ src/main/java/be/haexnet/sshcredentialmanagement/controller/IndexController.java
@@ -1,13 +1,31 @@
 package be.haexnet.sshcredentialmanagement.controller;
 
+import be.haexnet.sshcredentialmanagement.model.Credential;
+import be.haexnet.sshcredentialmanagement.service.ICredentialService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
+
+    private final ICredentialService credentialService;
+
+    @Autowired
+    public IndexController(final ICredentialService credentialService) {
+        this.credentialService = credentialService;
+    }
+
     @RequestMapping("/")
     public String index(final ModelMap modelMap) {
+        modelMap.put("credentials", findAllCredentials());
         return "index";
+    }
+
+    private List<Credential> findAllCredentials() {
+        return credentialService.findAll();
     }
 }
